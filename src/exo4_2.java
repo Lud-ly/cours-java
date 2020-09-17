5c119858e6677fe42e59d20bb604b9009fa831ca
Lud-ly <Ludom82@gmail.com>
2020-09-17 23:47:26 +0200

    algo java suite

--- src/exo4_2.java
+++ src/exo4_2.java
@@ -7,7 +7,6 @@ public class exo4_2 {
         int  h = s.nextInt();
         System.out.println("puis les minutes");
          int  m = s.nextInt();
-<<<<<<< HEAD
 
         m = m + 1;
         if (m == 60)
@@ -22,22 +21,4 @@ public class exo4_2 {
         System.out.println("dans une minute il est " + h + " heure "+ m + " minutes");
     }
  }
-
-
-=======
-        m = m + 1;
-        if (m == 60){
-            h = h+1;
-            m=0;
-
-        if (h ==24){
-            h=0;
-            }
-        }
-
-        System.out.println("dans une minute il est " + h + " heure "+ m + " minutes");
-        }
-
-    }
->>>>>>> b3c1b26a773891fa3667c819ef3e0b57845acacd
 
